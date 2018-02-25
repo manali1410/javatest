eeba171b4dd671b6fbb23d2767f489f04aff147d
manali1410 <manalijain1410@gmail.com>
2018-02-24 22:14:36 -0500

    k

---
+++ src/main/java/abhigyan/manali/App.java
@@ -0,0 +1,13 @@
+package abhigyan.manali;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
